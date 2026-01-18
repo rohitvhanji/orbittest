824a2be01ad78704d36d62b37b368e4a7b395685
rohitvhanji <56556783+rohitvhanji@users.noreply.github.com>
2026-01-18 16:24:35 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.CONFIG = {
+    SUPABASE_URL: 'https://xcdnyheqtagizlcyzuzb.supabase.co',
+    SUPABASE_KEY: 'sb_publishable_5cH3NNSKN1knnkm6ZMvggw_6RQdq9Nh',
+    TIMEZONEDB_KEY: 'VW4CCUCGOI2M'
+};
